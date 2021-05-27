505e2cb1574b70d4ca085beff337a853a0c68ca9
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-27 08:09:35 -0500

    Retornamos todos los Post, con la configuración del Recurso.

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
-    Route::apiResource('posts', App\Http\Controllers\Api\V1\PostController::class)->only('show');
+    Route::apiResource('posts', App\Http\Controllers\Api\V1\PostController::class)
+        ->only(['index', 'show']);
 });
