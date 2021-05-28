9272c09c17066130dd6aaf752e8f7fe7a0d9abbe
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-28 10:13:39 -0500

    Iniciamos la versión 2 del api.

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::prefix('v1')->group(function() {
-    Route::apiResource('posts', App\Http\Controllers\Api\V1\PostController::class)
+    Route::apiResource('posts', PostV1::class)
         ->only(['index', 'show', 'destroy']);
+});
+
+Route::prefix('v2')->group(function() {
+    Route::apiResource('posts', PostV2::class)
+        ->only(['index', 'show']);
 });
