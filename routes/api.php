d3a1e15ae408a2176d925326ec8ccc450b200c31
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-28 07:52:24 -0500

    Agregamos la funcionalidad de borrar un post desde la ruta del API

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('posts', App\Http\Controllers\Api\V1\PostController::class)
-        ->only(['index', 'show']);
+        ->only(['index', 'show', 'destroy']);
 });
