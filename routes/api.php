b46e058929d086ed131192c35bb6e61a1e06c8a8
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-31 08:03:06 -0500

    Agregamos la funcionalidad para iniciar sesión desde el Api.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
@@ -7,10 +8,14 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('posts', PostV1::class)
-        ->only(['index', 'show', 'destroy']);
+        ->only(['index', 'show', 'destroy'])
+        ->middleware('auth:sanctum');
 });
 
 Route::prefix('v2')->group(function() {
     Route::apiResource('posts', PostV2::class)
-        ->only(['index', 'show']);
+        ->only(['index', 'show'])
+        ->middleware('auth:sanctum');
 });
+
+Route::post('login', [LoginController::class, 'login']);
